4f7ea60797dd2253694f916fe33f6cab53b5265b
noloerino <jhshi07@gmail.com>
2020-09-15 12:41:48 -0700

    [lab03] Fix ex2 CC error

--- lab03/ex2.s
+++ lab03/ex2.s
@@ -33,8 +33,8 @@ main:
     addi sp, sp, -16
     sw s0, 0(sp)
     sw s1, 4(sp)
-    sw s2, 4(sp)
-    sw ra, 8(sp)
+    sw s2, 8(sp)
+    sw ra, 12(sp)
     # END PROLOGUE
     addi t0, x0, 0
     addi s0, x0, 0
@@ -64,8 +64,8 @@ exit:
     # BEGIN EPILOGUE
     lw s0, 0(sp)
     lw s1, 4(sp)
-    lw s2, 4(sp)
-    lw ra, 8(sp)
+    lw s2, 8(sp)
+    lw ra, 12(sp)
     addi sp, sp, 16
     # END EPILOGUE
     jr ra
