2aefda0eb81ac7f02f2e094217a772d8856ba20a
ryanthornton <rpthornton@berkeley.edu>
2020-11-09 09:14:35 -0800

    Added Lab 09 (#8)
    * starter code for lab09 added
    
    * change to lab09 test
    
    * rename of files for clarity
    
    * updated header
    
    * Lowered runtimes for AG

---
+++ lab09/common.c
@@ -0,0 +1,79 @@
+#include <time.h>
+#include <stdio.h>
+#include <x86intrin.h>
+#include "common.h"
+
+long long int sum(unsigned int vals[NUM_ELEMS]) {
+	clock_t start = clock();
+
+	long long int sum = 0;
+	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+		for(unsigned int i = 0; i < NUM_ELEMS; i++) {
+			if(vals[i] >= 128) {
+				sum += vals[i];
+			}
+		}
+	}
+	clock_t end = clock();
+	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+	return sum;
+}
+
+long long int sum_unrolled(unsigned int vals[NUM_ELEMS]) {
+	clock_t start = clock();
+	long long int sum = 0;
+
+	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+		for(unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+			if(vals[i] >= 128) sum += vals[i];
+			if(vals[i + 1] >= 128) sum += vals[i + 1];
+			if(vals[i + 2] >= 128) sum += vals[i + 2];
+			if(vals[i + 3] >= 128) sum += vals[i + 3];
+		}
+
+		//This is what we call the TAIL CASE
+		//For when NUM_ELEMS isn't a multiple of 4
+		//NONTRIVIAL FACT: NUM_ELEMS / 4 * 4 is the largest multiple of 4 less than NUM_ELEMS
+		for(unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) {
+				sum += vals[i];
+			}
+		}
+	}
+	clock_t end = clock();
+	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+	return sum;
+}
+
+long long int sum_simd(unsigned int vals[NUM_ELEMS]) {
+	clock_t start = clock();
+	__m128i _127 = _mm_set1_epi32(127);		// This is a vector with 127s in it... Why might you need this?
+	long long int result = 0;				   // This is where you should put your final result!
+	/* DO NOT DO NOT DO NOT DO NOT WRITE ANYTHING ABOVE THIS LINE. */
+	
+	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+		/* YOUR CODE GOES HERE */
+
+		/* You'll need a tail case. */
+
+	}
+	clock_t end = clock();
+	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+	return result;
+}
+
+long long int sum_simd_unrolled(unsigned int vals[NUM_ELEMS]) {
+	clock_t start = clock();
+	__m128i _127 = _mm_set1_epi32(127);
+	long long int result = 0;
+	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+		/* COPY AND PASTE YOUR sum_simd() HERE */
+		/* MODIFY IT BY UNROLLING IT */
+
+		/* You'll need 1 or maybe 2 tail cases here. */
+
+	}
+	clock_t end = clock();
+	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+	return result;
+}
